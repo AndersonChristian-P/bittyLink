ae7cbfdd811572e5c9cbafcccfcc25ac269d4b6b
Christian Anderson <ca96187@gmail.com>
2019-09-10 09:11:12 -0600

    added node-sass package and setup app for scss

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import './App.css';
+import Header from "./components/Header/Header"
+import './App.scss';
 
 function App() {
   return (
     <div>
-      <div>Jello World!!!</div>
-      <div>This is the start of my Bit.ly UI clone</div>
-      <div>Goal is to clone out the UI of Bit.ly for my site with the following mvp:</div>
-      <ul>
-        <li>Shorten links on the homepage</li>
-        <li>Register and login</li>
-        <li>As a registered user you can save your shortened links, modify saved links to get a new shortened link, and delete saved shortened links</li>
-        <li>The site will also be responsive</li>
-        <li>There will be cross-browser compatibility, accessibility (ADA compliance, ARIA (Accessible Rich Internet Applications) where applicable)</li>
-      </ul>
+      <Header />
     </div>
   );
 }
